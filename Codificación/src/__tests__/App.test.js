3e6e3d2c1167eb0333c6c8b45d2f34157479c2b2
Wílmer E. León Díaz <42726764+wilmereleon@users.noreply.github.com>
2024-10-19 17:49:04 -0500

    Prueba de Jest

--- Codificación/src/__tests__/App.test.js
+++ Codificación/src/__tests__/App.test.js
@@ -6,15 +6,3 @@ test('renders app component', () => {
   const appElement = screen.getByTestId('app');
   expect(appElement).toBeInTheDocument();
 });
-
-test('renders welcome message', () => {
-  render(<App />);
-  const welcomeMessage = screen.getByText('Welcome to the App!');
-  expect(welcomeMessage).toBeInTheDocument();
-});
-
-test('renders button', () => {
-  render(<App />);
-  const buttonElement = screen.getByRole('button');
-  expect(buttonElement).toBeInTheDocument();
-});
