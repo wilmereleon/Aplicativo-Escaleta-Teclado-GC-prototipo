bdd6a41c7a8979e29ecedbde94793865576b42e9
Wílmer E. León Díaz <42726764+wilmereleon@users.noreply.github.com>
2024-10-19 15:15:26 -0500

    Implementación de entorno de aplicación React
    Se añaden las características de React para el Frontend

---
+++ Codificación/ETgc/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="text-center">Hola, Mundo!</h1>
+      <button className="btn btn-primary">Haz clic aquí</button>
+    </div>
+  );
+}
+
+export default App;
